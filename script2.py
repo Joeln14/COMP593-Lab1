227f5035d19dd81aa1e7aea1537f35dbf1ab592d
Joeln14 <122818202+Joeln14@users.noreply.github.com>
2023-01-23 13:06:51 -0500

    modified both files in the repo clone
    -removed print lines from script1
    -added print line to script2

--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
 print("Simple is better than complex.")
+print("hop in the pond if youre going to act like a silly goose")
